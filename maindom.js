9da28aea416ebb5a3a159fa1f1eb1ecc1e776021
yasminemzoughi <yasminemzoughi4@gmail.com>
2022-11-22 19:40:12 +0100

    pannier-d'achat

---
+++ maindom.js
@@ -0,0 +1,75 @@
+//declaration
+var favs= Array.from(document.querySelectorAll(".fa-heart"));
+var trash=Array.from(document.querySelectorAll(".fa-trash-alt"));
+var cards=Array.from(document.querySelectorAll(".card"));
+var plusbtn= Array.from(document.querySelectorAll(".fa-plus-circle"));
+var minusbtn=Array.from(document.querySelectorAll(".fa-minus-circle"));
+
+var coun=Array.from(document.querySelectorAll(".qute"));
+
+
+//favorites
+for(let fav of favs){
+    fav.addEventListener("click",function(){
+        if (fav.style.color == "black"){
+            fav.style.color = "red";
+        }
+        else {
+            fav.style.color = "black";
+        }
+    });
+}
+//trash
+for (let i in trash){
+    trash[i].addEventListener("click", function(){
+        cards[i].remove();
+     total();   
+    });  
+}
+
+//add btn
+for (let plus of plusbtn){
+    plus.addEventListener("click",function(){
+        plus.nextElementSibling.innerHTML++;
+        total(); 
+});} 
+
+
+//minus btn
+let count=0;
+for (let moin in minusbtn) {
+    moin.addEventListener("click",function(){
+       coun.innerHTML--;
+        //count--;
+      //  moin.innerHTML=count;
+        total();
+    });}
+
+//total
+function total(){
+var qte=Array.from(document.querySelectorAll(".qute"));
+var price=Array.from(document.querySelectorAll(".unitt-price"));
+let s=0;
+for (i=0; i< price.length ; i++){
+     s = s + qte[i].innerHTML*price[i].innerHTML;    
+}
+     document.getElementById("total-price").innerHTML = s;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
